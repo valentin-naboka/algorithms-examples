69272b12f26aa266d2e7a95cfe01a7a0aacd77cf
Valentin Naboka <valentin.naboka@gmail.com>
2020-09-20 17:26:51 +0300

    Internal list state after push front tests have been added.

--- list/list_test.go
+++ list/list_test.go
@@ -9,19 +9,15 @@ import (
 	"github.com/algorithms-examples/testutil"
 )
 
-func printListStateIfFailed(t *testing.T, list *UnrolledForwardList) {
-	if t.Failed() {
-		var result string
-		for node := list.head; node != nil; node = node.next {
-			result += fmt.Sprintf("%v", node.values)
-		}
-		splitter := strings.Repeat("-", len(result))
-		t.Log(splitter)
-		t.Log(result)
-		t.Log(splitter)
+func checkValue(t *testing.T, expected, actual interface{}) {
+	if !reflect.DeepEqual(expected, actual) {
+		testutil.PrintCaller(t, 2)
+		t.Errorf("Expected: %d, Actual: %d\n", expected, actual)
 	}
 }
 
+// Singly linked list tests
+// ========================
 func testList(t *testing.T, expected []interface{}, actual *SignlyLinkedList) {
 	if actual.GetHead() == nil {
 		testutil.PrintCaller(t, 2)
@@ -39,15 +35,7 @@ func testList(t *testing.T, expected []interface{}, actual *SignlyLinkedList) {
 	}
 }
 
-func checkValue(t *testing.T, expected, actual interface{}) {
-	if !reflect.DeepEqual(expected, actual) {
-		testutil.PrintCaller(t, 2)
-		t.Errorf("Expected: %d, Actual: %d\n", expected, actual)
-	}
-}
-
-// Singly linked list tests
-// ========================
+//====> InsertAfter tests
 func TestInsertAfterEmptyList(t *testing.T) {
 	list := NewSinglyLinkedList()
 
@@ -59,7 +47,8 @@ func TestInsertAfterEmptyList(t *testing.T) {
 	testList(t, data, list)
 }
 
-func TestPushFron(t *testing.T) {
+//====> PushFront tests
+func TestPushFront(t *testing.T) {
 	list := NewSinglyLinkedList()
 
 	data := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
@@ -71,6 +60,7 @@ func TestPushFron(t *testing.T) {
 	testList(t, data, list)
 }
 
+//====> Init list tests
 func TestInitList(t *testing.T) {
 	data := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
 	list := NewSinglyLinkedList(data...)
@@ -85,6 +75,7 @@ func TestInitList(t *testing.T) {
 	}
 }
 
+//====> RemoveAfter tests
 func TestRemoveAfter(t *testing.T) {
 	data := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
 	list := NewSinglyLinkedList(data...)
@@ -101,6 +92,42 @@ func TestRemoveAfter(t *testing.T) {
 
 // Unrolled forward list tests
 // ============================
+func pushFront(list *UnrolledForwardList, data ...interface{}) {
+	for i := 0; i < len(data); i++ {
+		list.PushFront(data[i])
+	}
+}
+
+func createUnrolledForwardList(data []interface{}) *UnrolledForwardList {
+	list := NewUnrolledForwardList()
+	for i := len(data) - 1; i >= 0; i-- {
+		list.PushFront(data[i])
+	}
+	return list
+}
+
+func printListStateIfFailed(t *testing.T, list *UnrolledForwardList) {
+	if t.Failed() {
+		var result string
+		for node := list.head; node != nil; node = node.next {
+			result += fmt.Sprintf("%v", node.values)
+		}
+
+		headerName := " actual list "
+		resultLen := len(result)
+		headerNameLen := len(headerName)
+		if resultLen <= headerNameLen {
+			resultLen = headerNameLen + 6
+		}
+		headerFiller := strings.Repeat("-", (resultLen-len(headerName))/2)
+		header := headerFiller + headerName + headerFiller
+		t.Log(header)
+		t.Log(result)
+		footer := strings.Repeat("-", resultLen)
+		t.Log(footer)
+	}
+}
+
 func testUnrolledList(t *testing.T, expected []interface{}, actual *UnrolledForwardList) {
 	if actual.GetBegin() == nil {
 		testutil.PrintCaller(t, 2)
@@ -120,13 +147,32 @@ func testUnrolledList(t *testing.T, expected []interface{}, actual *UnrolledForw
 	printListStateIfFailed(t, actual)
 }
 
-func TestMoveToUnrolledList(t *testing.T) {
-	list := NewUnrolledForwardList()
-
-	data := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
-	for i := len(data) - 1; i >= 0; i-- {
-		list.PushFront(data[i])
+func testUnrolledForwardListInternals(t *testing.T, actual *UnrolledForwardList, expected ...[]interface{}) {
+	if actual.GetBegin() == nil {
+		testutil.PrintCaller(t, 2)
+		t.Errorf("list is empty")
 	}
+
+	node := actual.head
+	for _, expectedValues := range expected {
+		if node == nil {
+			t.Error("the end of the list reached early ")
+			break
+		}
+
+		if !reflect.DeepEqual(expectedValues, node.values) {
+			testutil.PrintCaller(t, 2)
+			t.Errorf("Expected: %d, Actual: %d\n", expectedValues, node.values)
+		}
+		node = node.next
+	}
+	printListStateIfFailed(t, actual)
+}
+
+//====> MoveTo tests
+func TestMoveToUnrolledList(t *testing.T) {
+	data := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+	list := createUnrolledForwardList(data)
 
 	for i := 1; i < len(data); i++ {
 		it := list.GetBegin().MoveTo(i)
@@ -142,17 +188,42 @@ func TestMoveToUnrolledList(t *testing.T) {
 	checkValue(t, data[3], it.GetValue())
 }
 
+//====> PushFront tests
 func TestPushFrontUnrolledList(t *testing.T) {
-	list := NewUnrolledForwardList()
-
 	data := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
-	for i := len(data) - 1; i >= 0; i-- {
-		list.PushFront(data[i])
-	}
+	list := createUnrolledForwardList(data)
 
 	testUnrolledList(t, data, list)
 }
 
+func TestPushFrontUnrolledListInternals(t *testing.T) {
+	data := []interface{}{4, 3, 2, 1}
+	list := createUnrolledForwardList(data)
+	testUnrolledForwardListInternals(t, list,
+		[]interface{}{4, 3, 2, 1})
+
+	pushFront(list, 5, 6, 7, 8)
+	testUnrolledForwardListInternals(t, list,
+		[]interface{}{8, 7, 6, 5, 4, 3, 2, 1})
+
+	pushFront(list, 9)
+	testUnrolledForwardListInternals(t, list,
+		[]interface{}{9, 8, 7, 6, 5},
+		[]interface{}{4, 3, 2, 1})
+
+	pushFront(list, 10, 11, 12)
+	testUnrolledForwardListInternals(t, list,
+		[]interface{}{12, 11, 10, 9, 8, 7, 6, 5},
+		[]interface{}{4, 3, 2, 1})
+
+	pushFront(list, 13, 14, 15, 16)
+	testUnrolledForwardListInternals(t, list,
+		[]interface{}{16, 15, 14, 13, 12, 11, 10, 9},
+		[]interface{}{8, 7, 6, 5},
+		[]interface{}{4, 3, 2, 1})
+}
+
+//====> InsertAfter tests
 func TestInsertAfterUnrolledList(t *testing.T) {
 	list := NewUnrolledForwardList()
 
@@ -222,15 +293,12 @@ func TestInsertAfterInMiddleUnrolledList(t *testing.T) {
 	testUnrolledList(t, []interface{}{1, 12, 2, 3, 4, 45, 5, 6, 7}, list)
 }
 
+//====> PopFront tests
 func TestPopFrontUnrolledList(t *testing.T) {
-	list := NewUnrolledForwardList()
-
 	data := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
-	dataLen := len(data)
-	for i := dataLen - 1; i >= 0; i-- {
-		list.PushFront(data[i])
-	}
+	list := createUnrolledForwardList(data)
 
+	dataLen := len(data)
 	for i := 1; i < dataLen; i++ {
 		value := list.PopFront()
 		checkValue(t, i, value)
@@ -238,14 +306,10 @@ func TestPopFrontUnrolledList(t *testing.T) {
 	}
 }
 
+//====> RemoveAfter tests
 func TestRemoveAfterUnrolledList(t *testing.T) {
-	list := NewUnrolledForwardList()
-
-	//TODO: copy-paste init list
 	data := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
-	for i := len(data) - 1; i >= 0; i-- {
-		list.PushFront(data[i])
-	}
+	list := createUnrolledForwardList(data)
 
 	for it := list.GetBegin(); list.GetLength() != 1; {
 		list.RemoveAfter(it)
